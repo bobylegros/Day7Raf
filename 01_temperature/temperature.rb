cb210cadd601cf67a717fede2a9f0e6d8d788a72
bobylegros <rafcapdeville@gmail.com>
2018-01-16 22:30:45 +0100

    Day 7 jusqu'à 02

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,11 @@
-#write your code here
+# def ftoc(num)
+#   num * (5/9)
+# end
+def ftoc(value_2_transforme_in_C)
+  (value_2_transforme_in_C - 32)* 5.0/9.0
+end
+
+def ctof(value_2_transforme_in_F)
+  value_2_transforme_in_F * 9.0 / 5.0 + 32
+end
+# temperature_celsius * 9.0 / 5.0 + 32
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,26 @@
 #write your code here
+def add (number_one, number_two)
+    number_one + number_two;
+end
+
+def subtract (number_one_sub, number_two_sub)
+    number_one_sub - number_two_sub;
+end
+
+def sum (numbers)
+    numbers.sum
+end
+
+def multiply(*args) # quelque soit le nombre d'argument ... cherchez "ruby splat operator" dans Google
+  args.inject(:*)   # https://apidock.com/ruby/Enumerable/inject ... j'applique l'opération 'multiplier' (d'où le '*') à chaque arguments (= chaque chiffre)
+end
+
+def power(num_1, num_2)
+  num_1 ** num_2
+end
+
+def factorial(number)
+  # https://apidock.com/ruby/Enumerable/reduce
+  # https://fr.wikipedia.org/wiki/Factorielle
+  (1..number).reduce(1, :*) # factorielle 0 valant 1 ... la valeur initiale du reduce est 1
+end
